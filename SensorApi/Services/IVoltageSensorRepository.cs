0c369fba8e3d2aaa6496425548fd6bfd6b6e864b
Piotr Krupa <piotrkrupa06@gmail.com>
2021-05-01 18:36:41 +0200

    Add basic voltage sensor repository

---
+++ SensorApi/Services/IVoltageSensorRepository.cs
@@ -0,0 +1,12 @@
+﻿using SensorApi.Data.Models;
+using System.Collections.Generic;
+
+namespace SensorApi.Services
+{
+    public interface IVoltageSensorRepository
+    {
+        void Add(double voltage, double error);
+        VoltageSensor GetLatest();
+        IEnumerable<VoltageSensor> List();
+    }
+}
---
+++ SensorApi/Services/VoltageSensorRepository.cs
@@ -0,0 +1,38 @@
+﻿using SensorApi.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorApi.Services
+{
+    public class VoltageSensorRepository : IVoltageSensorRepository
+    {
+        public VoltageSensorRepository()
+        {
+
+        }
+
+        public void Add(double voltage, double error)
+        {
+            var value = new VoltageSensor
+            {
+                Id = Guid.NewGuid().ToString(),
+                TimeStamp = DateTime.Now,
+                CurrentVoltage = voltage,
+                Error = error
+            };
+
+        }
+
+        public VoltageSensor GetLatest()
+        {
+            return null;
+        }
+
+        public IEnumerable<VoltageSensor> List()
+        {
+            return null;
+        }
+    }
+}
