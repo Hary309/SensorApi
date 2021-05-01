68b27ee85d00a77b8f9591650e974459ca4e90a7
Piotr Krupa <piotrkrupa06@gmail.com>
2021-05-01 18:36:13 +0200

    Add voltage sensor model

---
+++ SensorApi/Data/Models/VoltageSensor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorApi.Data.Models
+{
+    public class VoltageSensor
+    {
+        /// <summary>
+        /// Given id for the measurement
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Time of measurement
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>
+        /// Voltage of measurement
+        /// </summary>
+        public double CurrentVoltage { get; set; }
+
+        /// <summary>
+        /// Error of measuring device
+        /// </summary>
+        public double Error { get; set; }
+    }
+}
