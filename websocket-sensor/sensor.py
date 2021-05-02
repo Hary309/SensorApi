b9ba4e94ab82a5fcada4a60f5d9de2daad0f731d
Piotr Krupa <piotrkrupa06@gmail.com>
2021-05-02 19:02:40 +0200

    Add websocket sensor

---
+++ websocket-sensor/sensor.py
@@ -0,0 +1,34 @@
+from time import sleep
+from websocket import *
+import ssl
+import random
+import json
+
+ws = create_connection("wss://localhost:5001/voltage/ws",
+                       sslopt={"cert_reqs": ssl.CERT_NONE})
+
+
+def start_client():
+    error = random.random() / 1000
+    currentVoltage = random.random() * 1000
+
+    while ws.connected:
+        readings = {"Voltage": currentVoltage, "Error": error}
+
+        print(json.dumps(readings))
+        ws.send(json.dumps(readings))
+        sleep(0.1)
+
+        currentVoltage += (random.random() - 0.5) * 2.0
+
+    ws.close()
+
+
+def main():
+    try:
+        start_client()
+    except KeyboardInterrupt:
+        ws.close()
+
+
+main()
